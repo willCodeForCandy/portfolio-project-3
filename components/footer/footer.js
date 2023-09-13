1542602b319f1be1d59feda3b064f86e1f200fa1
willCodeForCandy <lavirgin@gmail.com>
2023-09-13 19:07:28 +0200

    fix image sources and add links to socials

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,13 +1,15 @@
 import "./footer.css";
 const $footer = document.querySelector("footer");
 
-const insta = "../public/assets/icons8-instagram-48.png";
-const linkedIn = "../public/assets/icons8-linkedin-48.png";
-const twitter = "../public/assets/icons8-twitter-squared-48.png";
-const github = "../public/assets/icons8-github-48.png";
+const insta = "../assets/icons8-instagram-48.png";
+const linkedIn = "../assets/icons8-linkedin-48.png";
+const twitter = "../assets/icons8-twitter-squared-48.png";
+const github = "../assets/icons8-github-48.png";
+const email = "../assets/icons8-email-48.png";
 
 $footer.innerHTML = `
-<div><a href="#"><img src="${linkedIn}" alt="my linkedIn"></a></div>
-<div><a href="#"><img src="${github}" alt="mu gitHub"></a></div>
-<div><a href="#"><img src="${twitter}" alt="my (not X)"></a></div>
-<div><a href="#"><img src="${insta}" alt="my Instagram"></a></div>`;
+<div><a href="https://www.linkedin.com/in/virginia-raschia-545196280/" target="_blank"><img src="${linkedIn}" alt="my linkedIn"></a></div>
+<div><a href="https://github.com/willCodeForCandy" target="_blank"><img src="${github}" alt="mu gitHub"></a></div>
+<div><a href="https://twitter.com/virginia7_9" target="_blank"><img src="${twitter}" alt="my (not X)"></a></div>
+<div><a href="https://www.instagram.com/lavirgin7/" target="_blank"><img src="${insta}" alt="my Instagram"></a></div>
+<div><a href="mailto:viraschia@gmail.com" target="_blank"><img src="${email}" alt="email me"></a></div>`;
